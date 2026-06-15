287a7b1dfd4cd7478d6742a43a8dbf970c5c6292
Tester Inet Doc <tester.inet.doc@gmail.com>
2026-06-15 17:00:15 +0700

    a

--- open.sh
+++ open.sh
@@ -4,8 +4,8 @@
 ##########################################################################
 ##########################################################################
 ##########################################################################
-                     API="ทดสอบ" 
-                     google_sheet="https://docs.google.com/spreadsheets/d/1H7UgGtAy3JKvulLqGXVm5zeNq1FdNKonMxxBETQtjXQ/edit?gid=1610289956#gid=1610289956" 
+                     API="template" 
+                     google_sheet="https://docs.google.com/spreadsheets/d/1Z93ZJv4TqTvpRfMFEw_hQRpBi-pmj6Z0PStQRmxEUME/edit?gid=1610289956#gid=1610289956" 
                      id="1"                 #เปลี่ยน id ทุกครั้งที่ยิง
                      user="1";            #จำนวนผู้ใช้งาน
                      duration="1";         #วินาที
